43df4f17d030f3afec5c4617c6893febe121c2d3
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-09 22:29:10 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,1 +1,22 @@
+import numpy as np
+# import pickle
+import joblib
+# from joblib import load
+import os
+import re
 
+# stop_words = set(stopwords.words('english'))
+# stemmer = PorterStemmer()
+
+def sanitize_input(user_input):
+    # Allow only alphanumeric characters, underscores, and dashes
+    return ''.join(c for c in user_input if c.isalnum() or c in ('_', '-'))
+
+
+def get_safe_path(base_dir, user_input):
+    sanitized_input = sanitize_input(user_input)
+    safe_path = os.path.join(base_dir, sanitized_input)
+    return os.path.abspath(safe_path)
+
+base_dir = os.getcwd()
+model_folder_path = get_safe_path(base_dir, 'NCB_project/resources')
