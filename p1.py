1085ba8cc1c48a31b5b3ae54e4f551fdbf9a9854
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-11 23:46:17 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -6,3 +6,5 @@ tfidf = TfidfVectorizer(max_features=5000, stop_words="english")
 X_tfidf = tfidf.fit_transform(X)
 
 
+# Save TF-IDF model for future use
+joblib.dump(tfidf, "tfidf_vectorizer_300_svm.pkl")
