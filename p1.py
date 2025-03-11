f7cc37995a022df8ef0533f7e7dd337f5bbaa220
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-12 00:01:13 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,10 +1,34 @@
 in.py
 
-# 2️⃣ Convert text to features using TF-IDF
-tfidf = TfidfVectorizer(max_features=5000, stop_words="english")
-# X_tfidf = tfidf.fit_transform(X.astype(str)).toarray()
-X_tfidf = tfidf.fit_transform(X)
+(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ conda list numpy
+# packages in environment at /home/ai_user/anaconda3/envs/ncb_nlp:
+#
+# Name                    Version                   Build  Channel
+numpy                     1.26.4           py39h5f9d8c6_0  
+numpy-base                1.26.4           py39hb5e798b_0  
+(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ conda install scikit-learn==1.6.0
+Collecting package metadata (current_repodata.json): done
+Solving environment: failed with initial frozen solve. Retrying with flexible solve.
+Collecting package metadata (repodata.json): done
+Solving environment: failed with initial frozen solve. Retrying with flexible solve.
+
+PackagesNotFoundError: The following packages are not available from current channels:
+
+  - scikit-learn==1.6.0
+
+Current channels:
+
+  - https://repo.anaconda.com/pkgs/main/linux-64
+  - https://repo.anaconda.com/pkgs/main/noarch
+  - https://repo.anaconda.com/pkgs/r/linux-64
+  - https://repo.anaconda.com/pkgs/r/noarch
+
+To search for alternate channels that may provide the conda package you're
+looking for, navigate to
+
+    https://anaconda.org
+
+and use the search bar at the top of the page.
 
 
-# Save TF-IDF model for future use
-joblib.dump(tfidf, "tfidf_vectorizer_300_svm.pkl")
+(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ 
