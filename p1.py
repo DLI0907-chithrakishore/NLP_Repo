ad6071a51ecffd57cbca8160cbc06c966f5687e9
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-11 21:56:54 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,25 +1,31 @@
 in.py
 
-(base) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ conda activate ncb_nlp
-(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ pip install -r API/requirements.txt
-error: externally-managed-environment
+(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ conda install --file API/requirements.txt
+Collecting package metadata (current_repodata.json): done
+Solving environment: failed with initial frozen solve. Retrying with flexible solve.
+Collecting package metadata (repodata.json): done
+Solving environment: failed with initial frozen solve. Retrying with flexible solve.
 
-× This environment is externally managed
-╰─> To install Python packages system-wide, try apt install
-    python3-xyz, where xyz is the package you are trying to
-    install.
-    
-    If you wish to install a non-Debian-packaged Python package,
-    create a virtual environment using python3 -m venv path/to/venv.
-    Then use path/to/venv/bin/python and path/to/venv/bin/pip. Make
-    sure you have python3-full installed.
-    
-    If you wish to install a non-Debian packaged Python application,
-    it may be easiest to use pipx install xyz, which will manage a
-    virtual environment for you. Make sure you have pipx installed.
-    
-    See /usr/share/doc/python3.12/README.venv for more information.
+PackagesNotFoundError: The following packages are not available from current channels:
 
-note: If you believe this is a mistake, please contact your Python installation or OS distribution provider. You can override this, at the risk of breaking your Python installation or OS, by passing --break-system-packages.
-hint: See PEP 668 for the detailed specification.
+  - fastapi==0.98.0
+  - uvicorn==0.22.0
+  - tensorflow==2.13.0
+  - unicorn==2.0.1.post1
+
+Current channels:
+
+  - https://repo.anaconda.com/pkgs/main/linux-64
+  - https://repo.anaconda.com/pkgs/main/noarch
+  - https://repo.anaconda.com/pkgs/r/linux-64
+  - https://repo.anaconda.com/pkgs/r/noarch
+
+To search for alternate channels that may provide the conda package you're
+looking for, navigate to
+
+    https://anaconda.org
+
+and use the search bar at the top of the page.
+
+
 (ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ 
