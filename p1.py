4f8bcc897648cfafd228bf394b15c0db41969db4
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-11 22:03:09 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,31 +1,33 @@
 in.py
 
-(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ conda install --file API/requirements.txt
-Collecting package metadata (current_repodata.json): done
-Solving environment: failed with initial frozen solve. Retrying with flexible solve.
-Collecting package metadata (repodata.json): done
-Solving environment: failed with initial frozen solve. Retrying with flexible solve.
+C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\python.exe D:\Bitbucket\NCB\nlp_intent_ncb\NCB_project\API\api.py 
+D:\Bitbucket\NCB\nlp_intent_ncb\NCB_project\API\resources
+Traceback (most recent call last):
+  File "D:\Bitbucket\NCB\nlp_intent_ncb\NCB_project\API\api.py", line 6, in <module>
+    intent_obj = intent_model.IntentModelLoad()
+  File "D:\Bitbucket\NCB\nlp_intent_ncb\NCB_project\API\intent_model.py", line 43, in __init__
+    self.model_ncb_le = joblib.load(pk_file)
+  File "C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\lib\site-packages\joblib\numpy_pickle.py", line 648, in load
+    obj = _unpickle(fobj)
+  File "C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\lib\site-packages\joblib\numpy_pickle.py", line 577, in _unpickle
+    obj = unpickler.load()
+  File "C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\lib\pickle.py", line 1212, in load
+    dispatch[key[0]](self)
+  File "C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\lib\site-packages\joblib\numpy_pickle.py", line 415, in load_build
+    self.stack.append(array_wrapper.read(self))
+  File "C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\lib\site-packages\joblib\numpy_pickle.py", line 252, in read
+    array = self.read_array(unpickler)
+  File "C:\Users\chithra.kishore\AppData\Local\anaconda3\envs\rnn\lib\site-packages\joblib\numpy_pickle.py", line 152, in read_array
+    array = pickle.load(unpickler.file_handle)
+ModuleNotFoundError: No module named 'numpy._core'
 
-PackagesNotFoundError: The following packages are not available from current channels:
-
-  - fastapi==0.98.0
-  - uvicorn==0.22.0
-  - tensorflow==2.13.0
-  - unicorn==2.0.1.post1
-
-Current channels:
-
-  - https://repo.anaconda.com/pkgs/main/linux-64
-  - https://repo.anaconda.com/pkgs/main/noarch
-  - https://repo.anaconda.com/pkgs/r/linux-64
-  - https://repo.anaconda.com/pkgs/r/noarch
-
-To search for alternate channels that may provide the conda package you're
-looking for, navigate to
-
-    https://anaconda.org
-
-and use the search bar at the top of the page.
-
-
-(ncb_nlp) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ 
+uvicorn==0.22.0
+fastapi==0.98.0
+unicorn==2.0.1.post1
+typing==3.7.4.3
+numpy==1.23.5
+tensorflow==2.13.0
+scikit-learn==1.2.0
+nltk==3.8.1
+joblib==1.2.0
+pytz
