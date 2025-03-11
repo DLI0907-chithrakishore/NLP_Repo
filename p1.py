a54679268ae84128c491997922651f293061e9d8
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-11 21:44:34 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,9 +1,3 @@
 in.py
 
-/usr/bin/python3 /home/ai_user/Chithra/nlp_intent_ncb/NCB_project/API/api.py
-(base) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ /usr/bin/python3 /home/ai_user/Chithra/nlp_intent_ncb/NCB_project/API/api.py
-Traceback (most recent call last):
-  File "/home/ai_user/Chithra/nlp_intent_ncb/NCB_project/API/api.py", line 27, in <module>
-    from fastapi import FastAPI,Request
-ModuleNotFoundError: No module named 'fastapi'
-(base) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project$ 
+chitra_venv) ai_user@ip-10-20-24-20:~/Chithra/nlp_intent_ncb/NCB_project/API$ python intent_model.py 
