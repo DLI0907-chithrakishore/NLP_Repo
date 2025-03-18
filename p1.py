33f40d6c0faa71e7e7ba7f31b9eabdf800a023d4
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-18 17:51:41 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,1 +1,7 @@
+print("Class distribution before resampling:", Counter(y_train))
+Class distribution before resampling: Counter({3: 1600, 2: 1600, 1: 928, 4: 169, 0: 147})
+# # 8️⃣ Train SVM Classifier  initially used
+svm_model = SVC(kernel="rbf", C=1.0, gamma=0.1,class_weight ='balanced', probability=True)#gamma="scale"
+svm_model.fit(X_train_smote, y_train_smote)
+
 
