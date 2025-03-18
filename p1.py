acec9c23d361933922b33ffc8c6ebdcc227e835c
DLI0907-chithrakishore <chithra.kishore@godigit.com>
2025-03-18 18:44:17 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -4,4 +4,20 @@ Class distribution before resampling: Counter({3: 1600, 2: 1600, 1: 928, 4: 169,
 svm_model = SVC(kernel="rbf", C=1.0, gamma=0.1,class_weight ='balanced', probability=True)#gamma="scale"
 svm_model.fit(X_train_smote, y_train_smote)
 
+from imblearn.over_sampling import RandomOverSampler
+from collections import Counter
+
+# Define desired class distribution
+desired_distribution = {3: 1600, 2: 1600, 1: 1600, 4: 1000, 0: 1000}
+
+# Initialize RandomOverSampler
+ros = RandomOverSampler(sampling_strategy=desired_distribution, random_state=42)
+
+# Resample the dataset
+X_train_resampled, y_train_resampled = ros.fit_resample(X_train, y_train)
+
+# Print new class distribution
+print("Class distribution after upsampling:", Counter(y_train_resampled))
+
+
 
